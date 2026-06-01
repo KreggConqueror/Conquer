17ab1c0761c7b2a2b6a6ad4c58ba44cc157cd321
Emma <emma.pena7500@gmail.com>
2026-05-31 20:55:20 -0600

    Cambio de ruta en HeroSection

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -10,7 +10,7 @@ export default function HeroSection() {
                 playsInline
                 className="hero-video"
             >
-                <source src="../../public/video/viltrumite.mp4" type="video/mp4" />
+                <source src="../../public/video/Viltrumite.mp4" type="video/mp4" />
             </video>
 
             <div className="hero-overlay">
